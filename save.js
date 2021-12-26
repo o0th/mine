4b0ca3033f975a9b0ba201da760b9c8cfbaf3fb2
o0th <o0th@pm.me>
2021-12-26 12:31:26 +0100

    basic save, more directions logic

---
+++ save.js
@@ -0,0 +1,55 @@
+export const Save = [
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  [
+    undefined,
+    { name: 'mine', model: 'mk1', output: 'right' },
+    { name: 'belt', model: 'mk1', input: 'left', output: 'right' },
+    { name: 'belt', model: 'mk1', input: 'left', output: 'right' },
+    { name: 'belt', model: 'mk1', input: 'left', output: 'right' },
+    { name: 'belt', model: 'mk1', input: 'left', output: 'right' },
+    { name: 'container', input: 'left' },
+    undefined,
+  ],
+  [
+    undefined,
+    { name: 'container', input: 'right' },
+    { name: 'belt', model: 'mk1', input: 'right', output: 'left' },
+    { name: 'belt', model: 'mk1', input: 'right', output: 'left' },
+    { name: 'belt', model: 'mk1', input: 'right', output: 'left' },
+    { name: 'belt', model: 'mk1', input: 'right', output: 'left' },
+    { name: 'mine', model: 'mk1', output: 'left' },
+    undefined
+  ],
+  [
+    undefined,
+    { name: 'mine', model: 'mk1', output: 'down' },
+    { name: 'container', input: 'down' },
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+  ],
+  [
+    undefined,
+    { name: 'belt', model: 'mk1', input: 'up', output: 'down' },
+    { name: 'belt', model: 'mk1', input: 'down', output: 'up' },
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+  ],
+  [
+    undefined,
+    { name: 'container', input: 'up' },
+    { name: 'mine', model: 'mk1', output: 'up' },
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
+  ],
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+]
